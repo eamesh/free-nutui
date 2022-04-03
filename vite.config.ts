a0627695f1080dfdb1841b0a2a8bbacba9f73329
ease <easeava@gmail.com>
2022-04-03 22:48:44 +0800

    chore: @nutui/nutui, naive-ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,32 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { resolve } from 'path';
+import { createStyleImportPlugin, NutuiResolve } from 'vite-plugin-style-import';
+
+const plugins = [
+  vue(),
+  vueJsx(),
+  createStyleImportPlugin({
+    resolves: [
+      NutuiResolve()
+    ]
+  })
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins,
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 配置 nutui 全局 scss 变量
+        additionalData: '@import "@nutui/nutui/dist/styles/variables.scss";'
+      }
     }
   }
 });
